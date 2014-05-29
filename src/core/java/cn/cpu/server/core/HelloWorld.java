8b5224f1c426c3f19b79a738b766fa1a58f251e9
caolisheng <c_lisheng@163.com>
2014-05-29 17:10:42 +0800

    在Ubuntu下测试GitEye。。。

---
+++ src/core/java/cn/cpu/server/core/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.cpu.server.core;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
